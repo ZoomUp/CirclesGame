f1700f0d8608bc90d6680cb69a21570ef0a6d374
Andrei Gushcha <andreymmf@gmail.com>
2022-03-07 13:55:08 +0300

    Main architecture created

---
+++ app/src/main/java/com/example/circlesgame/SettingsScreen.kt
@@ -0,0 +1,38 @@
+package com.example.circlesgame
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.circlesgame.databinding.FragmentSettingsScreenBinding
+
+class SettingsScreen : Fragment() {
+
+    private var _binding: FragmentSettingsScreenBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSettingsScreenBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
