ace5fa5eb209fa50fbe3450ca956eb5a40ac1654
Alexandr <bichcov@mail.ru>
2022-03-09 20:27:27 +0300

    delete App bar and visual fix

--- app/src/main/java/com/example/circlesgame/MainScreen.kt
+++ app/src/main/java/com/example/circlesgame/MainScreen.kt
@@ -1,25 +1,18 @@
 package com.example.circlesgame
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.graphics.drawable.toDrawable
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.circlesgame.databinding.FragmentMainScreenBinding
 import com.example.circlesgame.storages.SettingsStorage
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class MainScreen : Fragment() {
 
     private var _binding: FragmentMainScreenBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -34,14 +27,14 @@ class MainScreen : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.MainScreen.background = SettingsStorage.mainBackgroudColor.toDrawable()
-
-        binding.buttonStart.setOnClickListener {
-            findNavController().navigate(R.id.action_MainScreen_to_GameScreen)
-        }
-        binding.buttonSettings.setOnClickListener {
-            findNavController().navigate(R.id.action_MainScreen_to_settingsScreen)
+        binding.apply {
+            MainScreen.background = SettingsStorage.mainBackgroudColor.toDrawable()
+            buttonStart.setOnClickListener {
+                findNavController().navigate(R.id.action_MainScreen_to_GameScreen)
+            }
+            buttonSettings.setOnClickListener {
+                findNavController().navigate(R.id.action_MainScreen_to_settingsScreen)
+            }
         }
     }
 
