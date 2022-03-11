889fa392d3519a5c98db1b01a2ad29982af6d7f4
Alexandr <bichcov@mail.ru>
2022-03-11 21:23:18 +0300

    fix records page

--- app/src/main/java/com/example/circlesgame/RecordsScreen.kt
+++ app/src/main/java/com/example/circlesgame/RecordsScreen.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.circlesgame.adapters.RecordsAdapter
 import com.example.circlesgame.databinding.FragmentRecordsScreenBinding
+import com.example.circlesgame.storages.SettingsStorage
 
 class RecordsScreen : Fragment() {
 
@@ -29,16 +31,15 @@ class RecordsScreen : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
+            root.background = SettingsStorage.mainBackgroundColor.toDrawable()
             btnMenu.setOnClickListener {
                 findNavController().navigate(R.id.action_recordsScreen_to_MainScreen)
             }
             recyclerRecords.apply {
                 adapter = recordsAdapter
                 itemAnimator = null
-
             }
         }
-        recordsAdapter.notifyDataSetChanged()
     }
 
     override fun onDestroyView() {
